53717f04bf31c17844ce5290e26e577809c473b4
emilio.htp <emilio.htp@outlook.com>
2025-10-15 17:20:25 -0600

    update

--- config.py
+++ config.py
@@ -3,10 +3,8 @@ import os
 # Configuración de MongoDB usando variables de entorno
 MONGODB_URI = os.getenv("MONGODB_URI")
 DB_NAME = os.getenv("DB_NAME", "login")
-DB_NAME_PRUEBA = "prueba"  # Nueva base de datos
 COLLECTION_USERS = "users"
 COLLECTION_PRODUCTS = "products"
-COLLECTION_PRUEBA = "prueba_collection"  # Nueva colección
 
 # Validar que las variables de entorno estén configuradas
 if not MONGODB_URI:
